01525fd4b1f75367268d82d31599f750db810cc2
Ambrance <ambrance@mail.ru>
2019-03-22 23:00:19 +0300

    feat: pass all 100 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let includesCount = [];
+    for (let j = 2; j <= base; j++) {
+      let baseArr = [];
+      while (base % j == 0) {
+        baseArr.push(j);
+        base /= j;
+      }
+      if (baseArr.length !== 0) {
+        let res = 0;
+        for (let k = 1; number / Math.pow(baseArr[0], k) > 1; k++) {
+          res += Math.floor(number / Math.pow(baseArr[0], k));
+        }
+        includesCount.push(Math.floor(res / baseArr.length));
+      }
+    }
+    let zerosCount = Math.min(...includesCount);
+    return zerosCount;
 }
